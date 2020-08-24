ac357bf8a11cc909fe7a3c44652ad18911bdd348
shiven0612 <shiven.ec@gmail.com>
2020-08-24 21:09:16 +0530

    Updates

---
+++ precache-manifest.ee65a86bd4674763d46637a38758a1a5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2265fbd15074cb7de223677ac4a7085b",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "add9b883d7eb8802099d",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8fc7310e5776212e59d5",
+    "url": "/reactwebsite/static/css/main.0afcdec7.chunk.css"
+  },
+  {
+    "revision": "add9b883d7eb8802099d",
+    "url": "/reactwebsite/static/js/2.7b02c6c5.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.7b02c6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc7310e5776212e59d5",
+    "url": "/reactwebsite/static/js/main.b4bddc5c.chunk.js"
+  },
+  {
+    "revision": "eec87808e158f3f4700a",
+    "url": "/reactwebsite/static/js/runtime-main.824e2a87.js"
+  },
+  {
+    "revision": "12a61ee36492096148f5c2d4e38ca259",
+    "url": "/reactwebsite/static/media/about.12a61ee3.svg"
+  },
+  {
+    "revision": "941fa099e68e90579429638f0849a4d7",
+    "url": "/reactwebsite/static/media/android.941fa099.jpg"
+  },
+  {
+    "revision": "f9d2781e52713660efa386312c7450d6",
+    "url": "/reactwebsite/static/media/app.f9d2781e.jpg"
+  },
+  {
+    "revision": "48006761f4fe0ed02ec063a092ab3bf6",
+    "url": "/reactwebsite/static/media/digital.48006761.jpg"
+  },
+  {
+    "revision": "70ff7a69df9971e4519705d854530ed0",
+    "url": "/reactwebsite/static/media/home.70ff7a69.svg"
+  },
+  {
+    "revision": "2b8bdf44678c22c111057fe052123ebe",
+    "url": "/reactwebsite/static/media/marketing.2b8bdf44.jpg"
+  },
+  {
+    "revision": "2dd00d987a85d3bec58899a1ec35d8a7",
+    "url": "/reactwebsite/static/media/software.2dd00d98.jpg"
+  },
+  {
+    "revision": "12d6fddab0111a7fc16aee4b06d2bb1e",
+    "url": "/reactwebsite/static/media/web.12d6fdda.jpg"
+  }
+]);
